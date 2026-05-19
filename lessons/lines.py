c2fa316e8b8c413964864bfba497e0cb89a7d0c2
Taude;Demo <akuzminykh2010@gmail.com>
2026-05-19 20:35:21 +0300

    implemented colored lines

--- lessons/lines.py
+++ lessons/lines.py
@@ -1,8 +1,8 @@
 import pygame
+import math
 
 
-i = 0
-j = 0
+from lessons.color import hsv_to_srgb
 
 
 class Lines:
@@ -11,16 +11,43 @@ class Lines:
     def __init__(self, screen: pygame.Surface):
         width, height = screen.get_size()
         self.start_point = (width // 2, height // 2)
+        self.x = width - 1
+        self.y = height - 1
+        self.state = 'right'
 
     def draw(self, screen: pygame.Surface):
-        global i, j
+        x = self.x
+        y = self.y
+        state = self.state
+        step = 4
         width, height = screen.get_size()
-        pygame.draw.line(screen, 'black', self.start_point, (width - i, height - j))
-        if height >= j >= 0 and i == 0:
-            j += 4
-        elif j >= height and width >= i >= 0:
-            i += 4
-        elif (height >= j >= 0 or height + 4 >= j >= 0) and i >= width:
-            j -= 4
-        elif j <= height and (width >= i >= 0 or width + 4 >= i >= 0):
-            i -= 4
+        if state == '':
+            return
+        hue = math.degrees(math.atan2(y - self.start_point[1], x - self.start_point[0]))
+        color = hsv_to_srgb(hue, 1, 1)
+        pygame.draw.line(screen, color, self.start_point, (x, y))
+        if state == 'right':
+            y -= step
+            if y < 0:
+                state = 'top'
+                x = width - 1 + y
+                y = 0
+        elif state == 'top':
+            x -= step
+            if x < 0:
+                state = 'left'
+                y = -x
+                x = 0
+        elif state == 'left':
+            y += step
+            if y >= height:
+                state = 'bottom'
+                x = y - height + 1
+                y = height - 1
+        elif state == 'bottom':
+            x += step
+            if x >= width:
+                state = ''
+        self.state = state
+        self.x = x
+        self.y = y
